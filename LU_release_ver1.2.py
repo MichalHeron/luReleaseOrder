0959b6eb561c52f919262763c282cbb083829e23
michal <michklak91@gmail.com>
2025-02-03 13:30:22 +0100

    another version

--- LU_release_ver1.2.py
+++ LU_release_ver1.2.py
@@ -11,6 +11,7 @@ update_open_orders = set
 count_open_orders = 0
 orders_amount_on_begin = len(batch)
 first_lu = "WAN-000000"
+THE_LOWEST_AMOUNT_OF_OPENING_ORDERS = False #if true script pick this lu which belong to the lowest amount of orders in batch
 
 
 def get_indicates_of_order_containing_lu(main_list, target_value):
@@ -34,34 +35,70 @@ def delete_lu_from_lists(batch_list, item_to_delete):
 def next_lu_in_queue():
         min_list = [sublist for sublist in batch if len(sublist) == min(len(sublist) for sublist in batch)]
 
-        # print(min_list)
         lu_min_list = []
+        lu_min_list_with_biggest_amount_of_open_orders = []
+        lu_min_list_with_max_amount_of_belong_to_open_orders = []
+        lu_min_list_with_max_amount_of_not_belong_to_open_orders = []
         lu_batch_list = []
+
         for list_lu in min_list:
             for lu in list_lu:
                 lu_min_list.append(lu)
-        lu_min_list = list(set(lu_min_list))
+        lu_min_list_set = list(set(lu_min_list))
         for batch_lu in batch:
             for lu in batch_lu:
                 lu_batch_list.append(lu)
+        if THE_LOWEST_AMOUNT_OF_OPENING_ORDERS:
+            count = 99999
+        else:
+            count = -1
 
-        # count = len(set(lu_batch_list))
-        count = 999999999999999
-        ne_lu = "nic"
-        for lu in lu_min_list:
-            if count > lu_batch_list.count(lu):
-                count = lu_batch_list.count(lu)
+        old_index_count = 0
+        ne_lu = "empty"
+
+        for lu in lu_min_list_set:    #checking whicht lu belong to the most or less amount of orders
+            if count == lu_batch_list.count(lu):
+               lu_min_list_with_biggest_amount_of_open_orders.append(lu)
+            if THE_LOWEST_AMOUNT_OF_OPENING_ORDERS:
+               if count > lu_batch_list.count(lu):
+                    count = lu_batch_list.count(lu)
+                    lu_min_list_with_biggest_amount_of_open_orders = [lu]
+            else:
+                if count < lu_batch_list.count(lu):
+                   count = lu_batch_list.count(lu)
+                   lu_min_list_with_biggest_amount_of_open_orders = [lu]
+
+
+        for lu in lu_min_list_with_biggest_amount_of_open_orders: #checking from the last group which lu belong to a already opened oreders (has the most)
+            new_index_count = 0
+            lu_indicates = get_indicates_of_order_containing_lu(batch2, lu)
+            for index in lu_indicates:
+                if index in open_orders:
+                    new_index_count += 1
+            if new_index_count == old_index_count:
+                lu_min_list_with_max_amount_of_belong_to_open_orders.append(lu)
+            if new_index_count > old_index_count:
+                lu_min_list_with_max_amount_of_belong_to_open_orders = [lu]
+                old_index_count = new_index_count
+
+        count_closed_orders = 0
+        for lu in lu_min_list_with_max_amount_of_belong_to_open_orders: # if still has two or more will check wich one will close the most amount of orders
+            if count_closed_orders < lu_min_list.count(lu):
+                count_closed_orders = lu_min_list.count(lu)
                 ne_lu = lu
-            if count == lu_batch_list.count(lu): #additionall condition to keep consistency
+            if count_closed_orders == lu_batch_list.count(lu): #additionall condition to keep consistency - just pick this one with the lowest number in name
                 if lu < ne_lu:
                     ne_lu = lu
+
         return ne_lu
+
 
 orders_left = orders_amount_on_begin - len(batch)
 
 
 print("lu on sorter | opened orders at all | orders ready to pack | opened orders not ready to pack")
 while len(batch) > 0:
+    # print("@@@@@@@@@@@@@@@@@@@")
     print(len(queue), len(open_orders), orders_amount_on_begin - len(batch), len(open_orders) - orders_amount_on_begin + len(batch))
     next_lu = next_lu_in_queue()
     queue.append(next_lu)
