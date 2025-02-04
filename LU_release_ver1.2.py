a36ea3ee5abdec589bfbd225f737b5bc09f8ecad
michal <michklak91@gmail.com>
2025-02-04 10:30:45 +0100

    another version

--- LU_release_ver1.2.py
+++ LU_release_ver1.2.py
@@ -1,6 +1,5 @@
 import list_of_list_with_lu_generator as generator
 
-
 batch = generator.create_sorted_list_of_lists_containing_lu_by_orders()
 
 batch2 = batch.copy()
@@ -11,7 +10,7 @@ update_open_orders = set
 count_open_orders = 0
 orders_amount_on_begin = len(batch)
 first_lu = "WAN-000000"
-THE_LOWEST_AMOUNT_OF_OPENING_ORDERS = False #if true script pick this lu which belong to the lowest amount of orders in batch
+THE_LOWEST_AMOUNT_OF_OPENING_ORDERS = False  #if true script pick this lu which belong to the lowest amount of orders in batch
 
 
 def get_indicates_of_order_containing_lu(main_list, target_value):
@@ -22,84 +21,107 @@ def order_list_by_length(batch_list):
     return sorted(batch_list, key=len)
 
 
-def add_queue_item(batch_list): #old version
-    for i in batch_list[0]:
-        queue.append(i)
-
-
-
 def delete_lu_from_lists(batch_list, item_to_delete):
     return [[item for item in sublist if item != item_to_delete] for sublist in batch_list]
 
 
+def search_lu_which_belong_to_the_less_orders_at_all(lu_list_set, batch_list):
+    lu_min_list_with_lowest_amount_of_orders = []
+    count = 99999
+    for lu in lu_list_set:
+        if count == batch_list.count(lu):
+            lu_min_list_with_lowest_amount_of_orders.append(lu)
+        if count > batch_list.count(lu):
+            count = batch_list.count(lu)
+            lu_min_list_with_lowest_amount_of_orders = [lu]
+    return lu_min_list_with_lowest_amount_of_orders
+
+
+def search_lu_which_belong_to_the_most_orders_at_all(lu_list_set, batch_list):
+    lu_min_list_with_biggest_amount_of_orders = []
+    count = -1
+    for lu in lu_list_set:
+        if count == batch_list.count(lu):
+            lu_min_list_with_biggest_amount_of_orders.append(lu)
+        if count < batch_list.count(lu):
+            count = batch_list.count(lu)
+            lu_min_list_with_biggest_amount_of_orders = [lu]
+    return lu_min_list_with_biggest_amount_of_orders
+
+
+def check_which_lu_has_the_most_amount_of_already_opened_orders(lu_list):
+    lu_min_list_with_max_amount_of_belong_to_open_orders = []
+    old_index_count = 0
+    for lu in lu_list:  #checking from the last group which lu belong to a already opened oreders (has the most)
+        new_index_count = 0
+        lu_indicates = get_indicates_of_order_containing_lu(batch2, lu)
+        for index in lu_indicates:
+            if index in open_orders:
+                new_index_count += 1
+        if new_index_count == old_index_count:
+            lu_min_list_with_max_amount_of_belong_to_open_orders.append(lu)
+        if new_index_count > old_index_count:
+            lu_min_list_with_max_amount_of_belong_to_open_orders = [lu]
+            old_index_count = new_index_count
+    return lu_min_list_with_max_amount_of_belong_to_open_orders
+
+
+def convert_lu_min_list_as_set(min_list): # convert list of list of lu which belong to orders with minimum amount of lu to set of lu
+    lu_min_list = []
+    for list_lu in min_list:
+        for lu in list_lu:
+            lu_min_list.append(lu)
+    lu_min_list_set = list(set(lu_min_list))
+    return lu_min_list_set, lu_min_list
+
+
+def lu_batch_list_from_list_of_list(batch_to_convert):
+    lu_batch = []
+    for batch_lu in batch_to_convert:
+        for lu in batch_lu:
+            lu_batch.append(lu)
+    return lu_batch
+
+
 def next_lu_in_queue():
-        min_list = [sublist for sublist in batch if len(sublist) == min(len(sublist) for sublist in batch)]
+    min_list = [sublist for sublist in batch if len(sublist) == min(len(sublist) for sublist in batch)]
 
-        lu_min_list = []
-        lu_min_list_with_biggest_amount_of_open_orders = []
-        lu_min_list_with_max_amount_of_belong_to_open_orders = []
-        lu_min_list_with_max_amount_of_not_belong_to_open_orders = []
-        lu_batch_list = []
+    list_group = convert_lu_min_list_as_set(min_list)
+    lu_list_as_set = list_group[0]
+    lu_list = list_group[1]
 
-        for list_lu in min_list:
-            for lu in list_lu:
-                lu_min_list.append(lu)
-        lu_min_list_set = list(set(lu_min_list))
-        for batch_lu in batch:
-            for lu in batch_lu:
-                lu_batch_list.append(lu)
-        if THE_LOWEST_AMOUNT_OF_OPENING_ORDERS:
-            count = 99999
-        else:
-            count = -1
+    lu_batch_list = lu_batch_list_from_list_of_list(batch)
 
-        old_index_count = 0
-        ne_lu = "empty"
+    if THE_LOWEST_AMOUNT_OF_OPENING_ORDERS:
+        lu_list_as_set = search_lu_which_belong_to_the_less_orders_at_all(lu_list_as_set, lu_batch_list)
+    else:
+        lu_list_as_set = search_lu_which_belong_to_the_most_orders_at_all(lu_list_as_set, lu_batch_list)
 
-        for lu in lu_min_list_set:    #checking whicht lu belong to the most or less amount of orders
-            if count == lu_batch_list.count(lu):
-               lu_min_list_with_biggest_amount_of_open_orders.append(lu)
-            if THE_LOWEST_AMOUNT_OF_OPENING_ORDERS:
-               if count > lu_batch_list.count(lu):
-                    count = lu_batch_list.count(lu)
-                    lu_min_list_with_biggest_amount_of_open_orders = [lu]
-            else:
-                if count < lu_batch_list.count(lu):
-                   count = lu_batch_list.count(lu)
-                   lu_min_list_with_biggest_amount_of_open_orders = [lu]
+    lu_list_as_set = check_which_lu_has_the_most_amount_of_already_opened_orders(
+        search_lu_which_belong_to_the_less_orders_at_all(lu_list_as_set, lu_batch_list))
+
+    picked_lu = "empty"
 
 
-        for lu in lu_min_list_with_biggest_amount_of_open_orders: #checking from the last group which lu belong to a already opened oreders (has the most)
-            new_index_count = 0
-            lu_indicates = get_indicates_of_order_containing_lu(batch2, lu)
-            for index in lu_indicates:
-                if index in open_orders:
-                    new_index_count += 1
-            if new_index_count == old_index_count:
-                lu_min_list_with_max_amount_of_belong_to_open_orders.append(lu)
-            if new_index_count > old_index_count:
-                lu_min_list_with_max_amount_of_belong_to_open_orders = [lu]
-                old_index_count = new_index_count
+    count_closed_orders = 0
+    for lu in lu_list_as_set:  # if still has two or more will check wich one will close the most amount of orders
+        if count_closed_orders < lu_list.count(lu):
+            count_closed_orders = lu_list.count(lu)
+            picked_lu = lu
+        if count_closed_orders == lu_batch_list.count(
+                lu):  #additionall condition to keep consistency - just pick this one with the lowest number in name
+            if lu < picked_lu:
+                picked_lu = lu
 
-        count_closed_orders = 0
-        for lu in lu_min_list_with_max_amount_of_belong_to_open_orders: # if still has two or more will check wich one will close the most amount of orders
-            if count_closed_orders < lu_min_list.count(lu):
-                count_closed_orders = lu_min_list.count(lu)
-                ne_lu = lu
-            if count_closed_orders == lu_batch_list.count(lu): #additionall condition to keep consistency - just pick this one with the lowest number in name
-                if lu < ne_lu:
-                    ne_lu = lu
-
-        return ne_lu
+    return picked_lu
 
 
 orders_left = orders_amount_on_begin - len(batch)
 
-
 print("lu on sorter | opened orders at all | orders ready to pack | opened orders not ready to pack")
 while len(batch) > 0:
-    # print("@@@@@@@@@@@@@@@@@@@")
-    print(len(queue), len(open_orders), orders_amount_on_begin - len(batch), len(open_orders) - orders_amount_on_begin + len(batch))
+    print(len(queue), len(open_orders), orders_amount_on_begin - len(batch),
+          len(open_orders) - orders_amount_on_begin + len(batch))
     next_lu = next_lu_in_queue()
     queue.append(next_lu)
 
--- LU_release_ver2.py
+++ LU_release_ver2.py
@@ -2,6 +2,7 @@ import list_of_list_with_lu_generator as generator
 
 
 batch = generator.create_sorted_list_of_lists_containing_lu_by_orders()
+batch2 = batch.copy()
 queue = []
 set_of_open_orders = set()
 count_open_orders = 0
@@ -10,7 +11,8 @@ point = 0
 add_point_for_not_belong_to_order = 1
 minus_point_for_belong_to_order = 3
 minus_extra_point_for_last_lu_in_order = 5
-set_method_of_picking_first_lu = False  # if True the logic to pick is original if False is by list
+set_method_of_picking_first_lu = True  # if True the logic to pick is original if False is by list
+THE_LOWEST_AMOUNT_OF_OPENING_ORDERS = False  #if true script pick this lu which belong to the lowest amount of orders in batch
 
 
 def create_lu_list(batch):
@@ -50,19 +52,111 @@ def delete_lu_from_lists(batch_list, item_to_delete):
     # return [sublist for sublist in batch_list if item_to_delete not in sublist]
 
 
-def first_lu_in_queue(batch, lu_list_set):
+def first_lu_in_queue(batch_var, lu_list_set_var):
     first_lu_points = 100000
     first_lu = "WAN-000000"
-    for lu in lu_list_set:
-        lu_points = sum(get_lu_amount_of_order_containing_lu(batch, lu))
+    for lu in lu_list_set_var:
+        lu_points = sum(get_lu_amount_of_order_containing_lu(batch_var, lu))
         if lu_points < first_lu_points:
             first_lu_points = lu_points
             first_lu = lu
     return first_lu
 
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+def search_lu_which_belong_to_the_less_orders_at_all(lu_list_set, batch_list):
+    lu_min_list_with_lowest_amount_of_orders = []
+    count = 99999
+    for lu in lu_list_set:
+        if count == batch_list.count(lu):
+            lu_min_list_with_lowest_amount_of_orders.append(lu)
+        if count > batch_list.count(lu):
+            count = batch_list.count(lu)
+            lu_min_list_with_lowest_amount_of_orders = [lu]
+    return lu_min_list_with_lowest_amount_of_orders
 
-def first_lu_in_queue_by_list(batch_list):
-    return batch_list[0][0]
+
+def search_lu_which_belong_to_the_most_orders_at_all(lu_list_set, batch_list):
+    lu_min_list_with_biggest_amount_of_orders = []
+    count = -1
+    for lu in lu_list_set:
+        if count == batch_list.count(lu):
+            lu_min_list_with_biggest_amount_of_orders.append(lu)
+        if count < batch_list.count(lu):
+            count = batch_list.count(lu)
+            lu_min_list_with_biggest_amount_of_orders = [lu]
+    return lu_min_list_with_biggest_amount_of_orders
+
+
+def check_which_lu_has_the_most_amount_of_already_opened_orders(lu_list):
+    lu_min_list_with_max_amount_of_belong_to_open_orders = []
+    old_index_count = 0
+    for lu in lu_list:  #checking from the last group which lu belong to a already opened oreders (has the most)
+        new_index_count = 0
+        lu_indicates = get_indicates_of_order_containing_lu(batch2, lu)
+        for index in lu_indicates:
+            if index in set_of_open_orders:
+                new_index_count += 1
+        if new_index_count == old_index_count:
+            lu_min_list_with_max_amount_of_belong_to_open_orders.append(lu)
+        if new_index_count > old_index_count:
+            lu_min_list_with_max_amount_of_belong_to_open_orders = [lu]
+            old_index_count = new_index_count
+    return lu_min_list_with_max_amount_of_belong_to_open_orders
+
+
+def convert_lu_min_list_as_set(min_list): # convert list of list of lu which belong to orders with minimum amount of lu to set of lu
+    lu_min_list = []
+    for list_lu in min_list:
+        for lu in list_lu:
+            lu_min_list.append(lu)
+    lu_min_list_set = list(set(lu_min_list))
+    return lu_min_list_set, lu_min_list
+
+
+def lu_batch_list_from_list_of_list(batch_to_convert):
+    lu_batch = []
+    for batch_lu in batch_to_convert:
+        for lu in batch_lu:
+            lu_batch.append(lu)
+    return lu_batch
+
+
+
+
+# def first_lu_in_queue_by_list():
+#     return batch[0][0]
+
+def first_lu_in_queue_by_list():
+    min_list = [sublist for sublist in batch if len(sublist) == min(len(sublist) for sublist in batch)]
+
+    list_group = convert_lu_min_list_as_set(min_list)
+    lu_list_as_set = list_group[0]
+    lu_list = list_group[1]
+
+    lu_batch_list = lu_batch_list_from_list_of_list(batch)
+
+    if THE_LOWEST_AMOUNT_OF_OPENING_ORDERS:
+        lu_list_as_set = search_lu_which_belong_to_the_less_orders_at_all(lu_list_as_set, lu_batch_list)
+    else:
+        lu_list_as_set = search_lu_which_belong_to_the_most_orders_at_all(lu_list_as_set, lu_batch_list)
+
+    lu_list_as_set = check_which_lu_has_the_most_amount_of_already_opened_orders(
+        search_lu_which_belong_to_the_less_orders_at_all(lu_list_as_set, lu_batch_list))
+
+    picked_lu = "empty"
+
+
+    count_closed_orders = 0
+    for lu in lu_list_as_set:  # if still has two or more will check wich one will close the most amount of orders
+        if count_closed_orders < lu_list.count(lu):
+            count_closed_orders = lu_list.count(lu)
+            picked_lu = lu
+        if count_closed_orders == lu_batch_list.count(
+                lu):  #additionall condition to keep consistency - just pick this one with the lowest number in name
+            if lu < picked_lu:
+                picked_lu = lu
+
+    return picked_lu
 
 
 
@@ -92,8 +186,8 @@ if(set_method_of_picking_first_lu):
     set_of_open_orders = get_indicates_of_order_containing_lu(batch, first_lu_in_queue(batch, lu_list_set))
     queue.append(first_lu_in_queue(batch, lu_list_set))
 if(not set_method_of_picking_first_lu):
-    set_of_open_orders = get_indicates_of_order_containing_lu(batch, first_lu_in_queue_by_list(batch))
-    queue.append(first_lu_in_queue_by_list(batch))
+    set_of_open_orders = get_indicates_of_order_containing_lu(batch, first_lu_in_queue_by_list())
+    queue.append(first_lu_in_queue_by_list())
 
 
 lu_amount_remaining = lu_set_length - 1
@@ -124,6 +218,9 @@ while orders_amount_on_begin - count_finished_orders() > 0:
         lu_list_set.remove(0)
     lu_set_length = len(lu_list_set)
     empty_lists = [sublist for sublist in batch if len(sublist) == 0]
+    # print("empty list", empty_lists)
+    # print(len(empty_lists))
+    # print(set_of_open_orders)
     lu_amount_remaining = lu_set_length
     print(lu_amount_on_begin - lu_amount_remaining, count_open_orders, count_finished_orders(),
           count_open_orders - len(empty_lists))
